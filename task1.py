5c98cbf3008e16f7e511875da22f9a8f88c9b9e4
Amandus Reimer <amandus@reimer.nu>
2023-02-21 08:43:29 +0100

    Förenklat task2

--- task1.py
+++ task1.py
@@ -4,58 +4,92 @@ import sympy as sp
 from scipy.integrate import odeint
 
 def taska():
+    q_val = 8
+    rho = 0.5
     r, q, u = sp.symbols('r q u')
-    f = r+ r*u - r/q*u - r/q*u**2 - 1
-    solu = sp.solve(f, u)
-    sp.pprint(solu)
-#taska()
+    fp= sp.solve(r*u*(1-u/q)-u/(1+u), u)
+    u_star1_val = fp[0].subs([(q, q_val), (r, rho)])
+    u_star2_val = fp[1].subs([(q, q_val), (r, rho)])
+    u_star3_val = fp[2].subs([(q, q_val), (r, rho)])
+    
+    print(u_star1_val)
+    print(u_star2_val)
+    print(u_star3_val)
 
+taska()
 
 def initializeU(eps0, u0, L, t):
-    u = np.zeros((L, t), dtype=np.float16)
-    for eps in range(L):
-        u[eps, 0] = u0/(1+ np.exp( (eps+1) - eps0))
+    u = np.zeros((L, t))
+    eps = np.arange(1, L+1)
+    u[:, 0] = u0/(np.ones(L)+ np.exp( eps - eps0))
     return u
 
 def initializeU2(eps0, u0, L, t):
-    u = np.zeros((L, t), dtype=np.float32)
-    for eps in range(L):
-        u[eps, 0] = u0*np.exp(-((eps+1) - eps0)**2)
+    u = np.zeros((L, t))
+    eps = np.arange(1, L+1)
+    u[:, 0] = u0*np.exp(-np.power(eps - eps0,2))
     return u
+"""
+def updateU(old_u, r, q, L, t):
+    time_step = 0.01
+    for eps in range(L):
+        new_u[1:-2] = old_u[eps1] + time_step*(r*u[eps, t-1] - r/q*u[eps, t-1]**2 - u[eps, t-1]/(1+u[eps, t-1]))
+        if eps !=  0 and eps != L-1:
+            new_u[eps, t] = u[eps, t] + time_step*(u[eps + 1 , t-1] + u[eps - 1, t-1] - 2*u[eps, t-1] )
+        elif eps==0:
+            new_u[eps] = u[eps, t] + time_step*(u[1, t-1] - u[0, t-1])
+        else: 
+            new_u[eps] = u[eps, t] + time_step*(u[eps-1, t-1] - u[eps, t-1])
+    return old_u
+"""   
 
 def updateU(u, r, q, L, t):
+    time_step = 0.01
     for eps in range(L):
-        u[eps, t] = u[eps, t-1] + 0.1*(r*u[eps, t-1] - r/q*u[eps, t-1]**2 - u[eps, t-1]/(1+u[eps, t-1]))
+        u[eps, t] = u[eps, t-1] + time_step*(r*u[eps, t-1] - r/q*np.power(u[eps, t-1], 2) - u[eps, t-1]/(1+u[eps, t-1]))
         if eps !=  0 and eps != L-1:
-            u[eps, t] = u[eps, t] + 0.1*(u[eps + 1 , t-1] + u[eps - 1, t-1] - 2*u[eps, t-1] )
+            u[eps, t] = u[eps, t] + time_step*(u[eps + 1 , t-1] + u[eps - 1, t-1] - 2*u[eps, t-1] )
         elif eps==0:
-            u[eps, t] = u[eps, t] + 0.1*(u[1, t-1] - u[0, t-1])
+            u[eps, t] = u[eps, t] + time_step*(u[1, t-1] - u[0, t-1])
         else: 
-            u[eps, t] = u[eps, t] + 0.1*(u[eps-1, t-1] - u[eps, t-1])
+            u[eps, t] = u[eps, t] + time_step*(u[eps-1, t-1] - u[eps, t-1])
     return u
 
 
 def taskb():
-    r =1/2
+    r =0.5
     q = 8
     L = 100
     tmax = 100
     #du/dtau = rho*u - rho/q *u**2 - u/(1+u ) + du^2/dxi^2
-    u0_1 = -(1-q)/2 + np.sqrt(q- q/r + (1-q)**2/2) 
+    
+    # 1/2*(-1 + q + np.sqrt(2* q* (-2 + r) + r + q**2 *r)/np.sqrt(r))
+    #-(1-q)/2 + np.sqrt(q- q/r + (1-q)**2/2) 
     #(r*(q - 1) + np.sqrt(r*(q**2*r + 2*q*r - 4*q + r)))/(2*r)
-    u0_2 =  -(1-q)/2 - np.sqrt(q- q/r + (1-q)**2/2) 
-    u0_3 = 1.1*u0_2
+
+    # Our values
+    u0_1 = -(1- q)/2 + np.sqrt(((1- q)/2)**2 - (1/r-1))
+    u0_2 =  -(1- q)/2 - np.sqrt(((1- q)/2)**2 - (1/r-1))
+    
+   
     
     eps0_1 = 20
     eps0_2 = 50
     eps0_3 = 50
 
-    u_list = [u0_1, u0_2, u0_3]
+    # Our second values
+    u0_1 = 5.56155281280883
+    u0_2 = 1.43844718719117
+
+
+    u_list = [u0_1, u0_2, 1.1*u0_2]
     eps_list = [eps0_1, eps0_2, eps0_3]
+    
 
     for i in range(3):
         curr_eps = eps_list[i]
         curr_u = u_list[i]
+        
         u = initializeU(curr_eps, curr_u, L, tmax) 
     
     
@@ -69,7 +103,7 @@ def taskb():
         plt.colorbar()
         plt.show()
         for i in range(100):
-            plt.plot(range(L),u[:,i], label = 't = ' + str(i*0.01))
+            plt.plot(u[:,i], label = 't = ' + str(i*0.01))
         
         plt.show()
 
@@ -88,7 +122,7 @@ def taskc():
     for i in range(1):
         u = initializeU2(eps0, u_list[i], L, tmax) 
         for t in range(1, tmax):
-            u = updateU(u, r, q, L, t)
+            u[t] = updateU(u, r, q, L, t)
         print(u[:,1])
         plt.imshow(u)
         plt.title('$u(\\xi , \\tau)$, u0 = ' + str(np.round(u_list[i], 2)) )
--- task2.py
+++ task2.py
@@ -8,18 +8,26 @@ def UpdateConcentrations(grid, tStep, Du, Dv, L, a, b):
     return grid + tStep*Diffusion(grid, Du, Dv, L, a, b)
 
 def Diffusion(grid, Du, Dv, L, a, b):
-    dU = np.zeros((L, L))
-    dV = np.zeros((L, L))
+
+    u = np.array(grid[0,:,:])
+
+    uu = np.roll(u, shift =-1, axis  = 0)
+    ud = np.roll(u, shift =1, axis  = 0)
+    uf = np.roll(u, shift =1, axis  = 1)
+    ub = np.roll(u, shift =-1, axis  = 1)
+
+    v = np.array(grid[1,:,:])
+
+    vu = np.roll(v, shift =-1, axis  = 0)
+    vd = np.roll(v, shift =1, axis  = 0)
+    vf = np.roll(v, shift =1, axis  = 1)
+    vb = np.roll(v, shift =-1, axis  = 1)
+
     
-    for i in range(L):
-        for j in range(L):
-             
-            dU[i, j] = (a -(b+1)*grid[0, i, j] + grid[0, i, j]**2*grid[1, i, j] + Du*(grid[0, i, (j+1)%L] + 
-                        grid[0, i, (j-1)%L] + grid[0, (i+1)%L, j] + grid[0, (i-1)%L, j] - 4*grid[0, i, j]))
-            
-            dV[i, j] = (b*grid[0, i, j]- grid[0, i, j]**2*grid[1, i, j] + Dv*(grid[1, i, (j+1)%L] + 
-                        grid[1, i, (j-1)%L] + grid[1, (i+1)%L, j] + grid[1, (i-1)%L, j] - 4*grid[1, i, j]))
-    
+
+    dU = np.ones((L,L))*a - (b+1)*u + np.power(u, 2)*v + Du*(uf + ub+ ud + uu - 4*u)
+    dV = b*u- np.power(u, 2)*v + Dv* (vf + vb + vd + vu -4*v)
+
     return np.array([dU, dV])
 
 def TaskC():
@@ -35,16 +43,21 @@ def TaskC():
     initialValueV = b/a
     grid[0,:,:] =  (np.ones((L, L))+ np.random.normal(0, 0.1, (L, L)))*initialValueU
     grid[1,:,:] =  (np.ones((L, L))+ np.random.normal(0, 0.1, (L, L)))*initialValueV
-    Dv = Dvs[1]
+    Dv = Dvs[3]
+
+    # Dvs[0] sets range to 3
+    # Dvs[1] sets range to 4.5
+    # Dvs[2] sets range to 7.5
+    # Dvs[3] sets range to 12
     temp_u = np.zeros((L, L))
-    for i in range(tIter):
-        
+    i = 0
+    while i< tIter:        
         grid = UpdateConcentrations(grid, tStep, Du, Dv, L, a, b)
         if (temp_u==grid[0,:,:]).all():
             print("u is constant at time step " + str(i))
             plt.pcolor(grid[0,:,:])
             plt.colorbar()
-            plt.clim([0, 6])
+            plt.clim([2.9, 3.1])
             plt.title("Steady state reached at time t = " + str(0.01*(i+1)))
             plt.show()
             
@@ -52,19 +65,49 @@ def TaskC():
         temp_u = grid[0,:,:]
         if i%100 == 0:
             print(i)
-        if (i+1)%1000 == 0:
+        if (i+1)%5000 == 0:
             
             plt.pcolor(grid[0,:,:])
             plt.colorbar()
-            plt.clim([0, 6])
+            plt.clim([0, 12])
             plt.title("u at time step " + str(i+1))
             plt.show()
+        i+=1
             
-    
 TaskC()
 def Test():
-    a = np.array([[1,1,3],[4,5,6]])
-    b = np.array([[1,2,3],[4,5,6]])
-    print((a==b).all())
+    L = 3
+    a = 1
+    b = 2
+    Du = 2
+    Dv = 2
+    u = np.array([[1,2,3], [4,5,6], [7,8,9]])
+    
+    uu = np.roll(u, shift =-1, axis  = 0)
+    ud = np.roll(u, shift =1, axis  = 0)
+    uf = np.roll(u, shift =1, axis  = 1)
+    ub = np.roll(u, shift =-1, axis  = 1)
+    print(u)
+    print(ub)
+    print(uf)
+    print(uu)
+    print(ud)
+    
+    return
+    v = np.array([[2,2,2], [3,3,3], [4,4,4]])
+    vu = np.roll(u, shift =-1, axis  = 0)
+    vd = np.roll(u, shift =1, axis  = 0)
+    vf = np.roll(u, shift =1, axis  = 1)
+    vb = np.roll(u, shift =-1, axis  = 1)
 
+    dU = np.zeros((L,L))
+    dV = np.zeros((L,L))
+    print(a*np.ones((L,L)) + (b+1)*u )
+    print(Du*(uf + ub+ ud + uu - 4*u))
+    print(np.power(u, 2)*v)
+    dU = a*np.ones((L,L)) - (b+1)*u + np.power(u, 2)*v + Du*(uf + ub+ ud + uu - 4*u)
+    dV = b*u- np.power(u, 2)*v + Dv* (vf + vb + vd + vu -4*v)
+    print(dU)
+    print(dV)
+    
 #Test()
